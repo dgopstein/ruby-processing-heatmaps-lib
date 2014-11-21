51885d5b863625c58fe95a7bdad89b54f629960c
Dan Gopstein <dan@gopstein.com>
2014-11-21 13:36:20 -0500

    Add transparency and blob size

--- heatmaps.rb
+++ heatmaps.rb
@@ -1,17 +1,19 @@
 module Heatmaps
-  attr_accessor :heatmapBrush, :heatmapColors, :gradientMap, :heatmap, :clickmap, :maxValue
+  attr_accessor :heatmapBrush, :heatmapColors, :gradientMap, :alphaMask, :heatmap, :clickmap, :maxValue
   
-  def draw_heat(width, height, clicks, click_w, click_h)
+  def draw_heat(width, height, clicks, click_size)
     self.maxValue = 0
 
     # load image data:
     dir = File.dirname(File.expand_path(".", __FILE__))
     self.heatmapColors = loadImage(dir+'/heatmapColors.png')
     self.heatmapBrush = loadImage(dir+'/heatmapBrush.png')
+    heatmapBrush.resize(click_size, click_size)
     
     # create empty canvases:
     self.heatmap = createImage(width, height, Processing::App::ARGB);
     self.gradientMap = createImage(width, height, Processing::App::ARGB);
+    self.alphaMask = createImage(width, height, Processing::App::ARGB);
     # load pixel arrays for all relevant images
     heatmapColors.loadPixels();
 
@@ -27,18 +29,22 @@ module Heatmaps
       # update the heatmap from the updated gradientMap:
       updateHeatmap();
     end
+
+    heatmap
   end
   
   
   # Rendering code that blits the heatmapBrush onto the gradientMap, centered at the specified pixel and drawn with additive blending
   
   def drawToGradient(x, y)
+    brushWidth = heatmapBrush.width
+    brushHeight = heatmapBrush.height
     # find the top left corner coordinates on the target image
-    startX = x-heatmapBrush.width/2;
-    startY = y-heatmapBrush.height/2;
+    startX = x-brushWidth/2;
+    startY = y-brushHeight/2;
   
-    (0...heatmapBrush.height).each do |py|
-      (0...heatmapBrush.width).each do |px|
+    (0...brushHeight).each do |py|
+      (0...brushWidth).each do |px|
         # for every pixel in the heatmapBrush:
         
         # find the corresponding coordinates on the gradient map:
@@ -50,7 +56,7 @@ module Heatmaps
         next if (hmX < 0 || hmY < 0 || hmX >= gradientMap.width || hmY >= gradientMap.height)
         
         # get the color of the heatmapBrush image at the current pixel.
-        col = heatmapBrush.pixels[py*heatmapBrush.width+px]; # The py*heatmapBrush.width+px part would normally also be optimized by just incrementing the index.
+        col = heatmapBrush.pixels[py*brushWidth+px]; # The py*brushWidth+px part would normally also be optimized by just incrementing the index.
         col = col & 0xff; # This eliminates any part of the heatmapBrush outside of the blue color channel (0xff is the same as 0x0000ff)
         
         # find the corresponding pixel image on the gradient map:
@@ -74,15 +80,19 @@ module Heatmaps
       # get the pixel's value. Note that we're not extracting any channels, we're just treating the pixel value as one big integer.
       # cast to float is done to avoid integer division when dividing by the maximum value.
       gmValue = gradientMap.pixels[i].to_f;
+      gmScaled = gmValue/maxValue
       
       # color map the value. gmValue/maxValue normalizes the pixel from 0...1, the rest is just mapping to an index in the heatmapColors data.
-      colIndex = ((gmValue/maxValue)*(heatmapColors.pixels.length-1)).to_i;
+      colIndex = (gmScaled*(heatmapColors.pixels.length-1)).to_i;
       col = heatmapColors.pixels[colIndex];
-  
+      alpha = [10 * gmScaled * 0xFF, 0xFF].min
+
       # update the heatmap at the corresponding position
-      heatmap.pixels[i] = col;
+      heatmap.pixels[i] = col
+      alphaMask.pixels[i] = alpha
     end
     # load the updated pixel data into the PImage.
-    heatmap.updatePixels();
+    heatmap.updatePixels()
+    heatmap.mask(alphaMask)
   end
 end 
